9ec4b248eaff73049b04f8935b120c118b79b275
Alex Lin <lib151886@gmail.com>
2021-08-30 00:16:43 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <hr>
+    <button class="btn btn-primary">確定</button>
+    <button class="btn btn-success">使用-bootstrap</button>
+    <button class="btn btn-danger">注意</button>
+    <div>
+        <b-button variant="danger">Button</b-button>
+        <b-button variant="success">使用-bootstrap-vue</b-button>
+        <b-button variant="outline-primary">Button</b-button>
+    </div>
+    <hr>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.btn{
+  margin: 10px;
+}
+</style>
